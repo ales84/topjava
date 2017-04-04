3a7551e1f19ae0685b501313fcd1a51dfd536d79
Anatoliy <tolyaskrg@gmail.com>
2017-04-04 11:11:53 +0600

    IsBetween Generic

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -11,8 +11,6 @@ import java.time.format.DateTimeFormatter;
  */
 public class DateTimeUtil {
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
     public static boolean isBetweenTime(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
@@ -20,6 +18,10 @@ public class DateTimeUtil {
 
     public static boolean isBetweenDate(LocalDate ld, LocalDate startDate, LocalDate endDate) {
         return ld.compareTo(startDate) >= 0 && ld.compareTo(endDate) <= 0;
+    }
+
+    public static <T> boolean isBetween(Comparable<T> local, Comparable<T> start, Comparable<T> end) {
+        return local.compareTo((T) start) >= 0 && local.compareTo((T) end) <= 0;
     }
 
     public static String toString(LocalDateTime ldt) {
