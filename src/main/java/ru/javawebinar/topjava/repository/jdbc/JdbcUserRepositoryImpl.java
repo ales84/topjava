0c2d4a3746fb62276449ce7c40c6ab0b34713883
Anatoliy <tolyaskrg@gmail.com>
2017-05-04 14:32:48 +0600

    JDBC Fetch Roles

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -6,7 +6,6 @@ import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -24,6 +23,8 @@ import java.util.*;
 @Repository
 @Transactional(readOnly = true)
 public class JdbcUserRepositoryImpl implements UserRepository {
+
+    private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -49,6 +50,14 @@ public class JdbcUserRepositoryImpl implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
+
+            for (Role role : user.getRoles()) {
+                Map<String, Object> paramMap = new HashMap<>();
+                paramMap.put("user_id", user.getId());
+                paramMap.put("role", role.toString());
+                namedParameterJdbcTemplate.update("INSERT INTO user_roles (user_id,role) values (:user_id, :role)", paramMap);
+            }
+
         } else {
             namedParameterJdbcTemplate.update(
                     "UPDATE users SET name=:name, email=:email, password=:password, " +
@@ -79,6 +88,9 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 
     @Override
     public List<User> getAll() {
+/*
+        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+*/
         return jdbcTemplate.query("SELECT * FROM users INNER JOIN user_roles ON users.id = user_roles.user_id ORDER BY name, email",
                 new UsersQueryResultSetExtractor());
     }
@@ -86,7 +98,7 @@ public class JdbcUserRepositoryImpl implements UserRepository {
     private class UsersQueryResultSetExtractor implements ResultSetExtractor<List<User>> {
         @Override
         public List<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
-            Map<Integer, User> userMap = new HashMap<>();
+            Map<Integer, User> userMap = new LinkedHashMap<>();
             while (rs.next()) {
                 Integer userId = rs.getInt("id");
                 User user = userMap.get(userId);
